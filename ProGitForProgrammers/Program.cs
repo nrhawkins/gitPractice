ee8c46e6116628f4500478f6f928d71ca620dd57
Natalie Hawkins <nrhawkins@gmail.com>
2022-06-20 23:39:08 -0700

    First commit - from command line

---
+++ ProGitForProgrammers/Program.cs
@@ -0,0 +1,12 @@
+
+namespace ProGitForProgrammers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I just added this in VS");       
+        }
+    }
+}
